9e6157745f503da977ff1e02b43c3373d7de844f
MSmit99 <144396430+MSmit99@users.noreply.github.com>
2023-09-07 19:46:56 -0400

    Update Events.jsx
    Yo Yo Yo

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -10,7 +10,7 @@ const events = [
   {
     name: 'Interest Meeting 01',
     description:
-      'Join us for our upcoming interest meetings! The Computer Science Club is excited to announce that we will be hosting multiple meetings each week, each with a different focus. We want to find the best times for our potential members, so we\'re offering several options. Come and learn more about what our club has to offer and how you can get involved. We hope to see you there!',
+      'Join us for our upcoming interest meetings! The Computer Science Club is excited to announce that we will be hosting multiple meetings each week, each with a different focus. We want to find the best times for our potential members, so we\'re offering several options. Come and learn more about what our club has to offer and how you can get involved. We hope to see you there!!!!',
     startDate: date('2023-08-28 19:00'),
     endDate: date('2023-08-28 20:00'),
     location: 'E281',
