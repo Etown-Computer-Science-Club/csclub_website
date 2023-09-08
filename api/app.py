d8ca04131d3890f03359aa5296761e97c3ba116a
SamH477 <142943760+SamH477@users.noreply.github.com>
2023-09-07 20:29:33 -0400

    update app file

--- api/app.py
+++ api/app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 # define a route like so:
 # @app.route(PATH HERE)
 # Right below the route, define a function that returns a message
+@app.route("/")
+def function():
+    return "Hello"
 
 
 if __name__ == '__main__':
