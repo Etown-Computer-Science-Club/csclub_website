326e4639919a4a69eddaae87367d933203ece933
Ethan Lajeunesse <ethan@lajfam.com>
2023-11-01 23:31:23 -0400

    Update redirectMap.js

--- client/src/redirects/redirectMap.js
+++ client/src/redirects/redirectMap.js
@@ -1,5 +1,5 @@
 const redirectMap = {
-  git: 'https://elizabethtown-my.sharepoint.com/:p:/g/personal/lajeunessee_etown_edu/ESRLu3ZcmZ5PvorjWAcA7uABKuz3y6NT4aWqSlscvZ81Bw',
+  git: 'http://elizabethtown-my.sharepoint.com/:p:/g/personal/lajeunessee_etown_edu/ESRLu3ZcmZ5PvorjWAcA7uABKuz3y6NT4aWqSlscvZ81Bw',
   react: 'https://elizabethtown-my.sharepoint.com/:p:/g/personal/lajeunessee_etown_edu/EemL9ifrHLlGv-MGBmEWR7QBBzNN6up6cvp8ARAw4Wb-VQ',
   ppaic: 'https://elizabethtown-my.sharepoint.com/:p:/g/personal/lajeunessee_etown_edu/EZXrX4ji8YNNkepKsHMUt94B6tjVgXM0n6I2nschtXYoVg?e=iREdk4',
 };
