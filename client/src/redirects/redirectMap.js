2d48ce34dda3d531738d69fa3213d1558129ad1c
Alexander Fox <foxa1@etown.edu>
2024-03-19 14:37:52 -0400

    ml

--- client/src/redirects/redirectMap.js
+++ client/src/redirects/redirectMap.js
@@ -5,7 +5,7 @@ const redirectMap = {
   firestore: 'https://elizabethtown-my.sharepoint.com/:p:/g/personal/lajeunessee_etown_edu/EUABZmk-m_9GsuzN-EfBla4BXiKeCk-3Nb5Cqo-tf0CyLg',
   asd: 'https://elizabethtown-my.sharepoint.com/:p:/g/personal/lajeunessee_etown_edu/Ea8ugI9RL95PpEcjqSQmhEsB22Mnco1ad0j96zukAcExkg?e=DGlWf4',
   gpt: 'https://elizabethtown-my.sharepoint.com/:p:/g/personal/islamm_etown_edu/EfWkNvwn5OZLtyo65XB-qgUBRq0EI0c_-af4v7__WCZ_bw',
-  machineLearning: 'https://elizabethtown-my.sharepoint.com/:p:/r/personal/foxa1_etown_edu/Documents/Machine%20Learning.pptx?d=w2bc1437cd3f0408592c7eed35ae33e3b&csf=1&web=1&e=nh0JJl',
+  ml: 'https://elizabethtown-my.sharepoint.com/:p:/r/personal/foxa1_etown_edu/Documents/Machine%20Learning.pptx?d=w2bc1437cd3f0408592c7eed35ae33e3b&csf=1&web=1&e=nh0JJl',
 
 };
 
